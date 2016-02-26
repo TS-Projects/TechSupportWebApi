d9944ef964c527f34840866c63f8e906c6ff4b99
Nayden Kirov <nayden.kirov@gmail.com>
2016-02-27 00:33:56 +0200

    :) edded service orders and search directive

---
+++ Source/WebClient/Web.Client/app/components/service/service-search-directive.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    var serviceSearchDirective = function serviceSearchDirective() {
+        return {
+            restrict: 'A',
+            templateUrl: 'app/components/service/service-search-directive.html',
+            scope: {
+                orders: '='
+            },
+            link: function (scope, element) {
+
+            }
+        };
+    };
+
+    angular
+        .module('techSupportApp.directives')
+        .directive('serviceSearchDirective', ['jQuery', serviceSearchDirective]);
+}());
